d65269f2212b51cb2b5c3b8d3ad48ac06a307c27
Mike Rapa <mike@mikerapa.com>
2021-10-09 11:06:05 -0400

    Fixed command arguments
    Added reference to flags package and logic to get arguments. Simplified code for processing positional argument.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/fatih/color"
 	"sls/fileTree"
@@ -8,18 +9,47 @@ import (
 )
 
 func main() {
-	const (initialPath = "/home/mike/go/src/sls"
-	pattern = "*.go")
-	tree := fileTree.GetFileTree(initialPath, pattern)
-	for _, tv := range tree{
-		printDirectory(tv, pattern)
-	}
-	//
-	//printHighlightText("I come FROM a land downunder", "down*own")
-	//printHighlightText("this was the file we were talking about", "thi*file")
 
-	//println(markStrings("sample text", []string{"sample text"}))
+	var (
+		showHelp bool
+		path string
+		filterPattern string
+	)
+	flag.BoolVar(&showHelp, "help", false, "show the help text")
+	flag.BoolVar(&showHelp, "h", false, "show the help text (shorthand)")
+	flag.StringVar(&path, "path", ".", "directory path for search")
+	flag.StringVar(&path, "p", ".", "directory path for search (shorthand)")
+	flag.StringVar(&filterPattern, "filter", "", "text to use as a filter pattern. Use quotes if text contains the * wildcard")
+	flag.StringVar(&filterPattern, "f", "", "text to use as a filter pattern. Use quotes if text contains the * wildcard")
+
+
+	flag.Parse()
+	if showHelp{
+		// TODO show the positional arguments
+		fmt.Println("sls [OPTION] [FILTER PATTERN]")
+		flag.PrintDefaults()
+	}
+
+	fmt.Printf("unused args: %v\n", flag.NArg())
+	for i,a:= range flag.Args(){
+		fmt.Printf("argument %d: %s\n", i, a)
+	}
+
+	// process positional argument for filter pattern
+	if filterPattern=="" && flag.NArg()>0{
+		filterPattern = flag.Args()[0]
+	}
+	//fmt.Printf("path: %s, pattern: %s\n", path, filterPattern)
+
+	// show  the results
+	tree := fileTree.GetFileTree(path, filterPattern)
+	for _, tv := range tree{
+		printDirectory(tv, filterPattern)
+	}
+
+
 }
+
 
 func printDirectory(directory fileTree.Directory, filterString string){
 	// only print the directory name if there are files under it
